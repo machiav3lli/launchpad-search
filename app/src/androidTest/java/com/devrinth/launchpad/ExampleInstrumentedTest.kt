b4e7e3e39e68a7cd5e74e898eb54c9fb71df2ec9
jaxparrow07 <zinczorphin@gmail.com>
2025-07-23 11:44:49 +0530

    chore: unknown IDE change to test

--- app/src/androidTest/java/com/devrinth/launchpad/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/devrinth/launchpad/ExampleInstrumentedTest.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.devrinth.launchpad.search.plugins.CalculatorPlugin
-import com.devrinth.launchpad.search.plugins.DefinitionPlugin
 import com.devrinth.launchpad.search.plugins.UnitConversionPlugin
 
 import org.junit.Test
