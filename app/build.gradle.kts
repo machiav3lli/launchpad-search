885761b4f6d0ff2a071091d5bedb77514ae59afb
jaxparrow07 <zinczorphin@gmail.com>
2025-07-23 11:44:02 +0530

    chore(refactor): change to static versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,11 @@ android {
             )
         }
     }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
